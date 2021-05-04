5dab786694eb4b4b643db4b219d91bb389aa0658
Divyanshi Agrawal <divsagr3@gmail.com>
2021-05-04 20:34:25 +0530

    Styling pokemon component

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -3,7 +3,24 @@ import React from "react";
 function Pokemon({ pokemon, key }) {
   return (
     <div className="pokemon">
-      <p>{pokemon.name}</p>
+      <div className="pokemon__name">
+        <p>{pokemon.name}</p>
+      </div>
+
+      <div className="pokemon__meta">
+        <span>{pokemon.maxHP}</span>
+        <span>{pokemon.maxCP}</span>
+      </div>
+
+      <div className="pokemon__image">
+        <img src={pokemon.image} alt={pokemon.name} />
+      </div>
+
+      <div className="pokemon__attacks">
+        {pokemon.attacks.special.slice(0, 3).map((attack) => (
+          <span key={`${attack.name}-${attack.damage}`}>{attack.name}</span>
+        ))}
+      </div>
     </div>
   );
 }
